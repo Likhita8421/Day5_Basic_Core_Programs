592b42de5d74df3d7c89b7c0b05b00bd124c0f16
HP <leenupandagre24@gmail.com>
2022-05-24 00:23:28 +0530

    Power Of Two added

---
+++ Power-Of-Two/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+static bool isPowerOfTwo(int n)
+{
+    if (n == 0)
+        return false;
+
+    while (n != 1)
+    {
+        if (n % 2 != 0)
+            return false;
+
+        n = n / 2;
+    }
+    return true;
+}
+
+{
+    Console.WriteLine(isPowerOfTwo(31) ? "Yes" : "No");
+    Console.WriteLine(isPowerOfTwo(64) ? "Yes" : "No");
+}
