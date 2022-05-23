83d09c03f3d58d2606439141fb0bbdc6aaade38c
HP <leenupandagre24@gmail.com>
2022-05-24 00:18:27 +0530

    Leap Year added

---
+++ LeapYear/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter Year : ");
+int Year = int.Parse(Console.ReadLine());
+if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0)) Console.WriteLine("{0} is a Leap Year.", Year);
+else Console.WriteLine("{0} is not a Leap Year.", Year);
+Console.ReadLine();
