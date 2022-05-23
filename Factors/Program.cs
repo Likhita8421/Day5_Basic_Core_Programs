50a74e995428f70e18719da27fd324654f5fd6a6
HP <leenupandagre24@gmail.com>
2022-05-24 00:31:42 +0530

    Factors added

---
+++ Factors/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int num, x;
+Console.WriteLine("Enter the Number ");
+num = int.Parse(Console.ReadLine());
+Console.WriteLine("The Factors are : ");
+for (x = 1; x <= num; x++)
+{
+    if (num % x == 0)
+    {
+        Console.WriteLine(x);
+    }
+}
+Console.ReadLine();
