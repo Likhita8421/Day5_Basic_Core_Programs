6fe35d5e8517b0a8ccf2253a1d5e37cf4c5e2518
HP <leenupandagre24@gmail.com>
2022-05-24 00:12:56 +0530

    Flip Coin added

---
+++ FlipCoin/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Welcome to Flip Coin Percentage Program ");
+int count = 0;
+int tails = 0;
+int heads = 0;
+
+while (count < 100)
+{
+    Random rnd = new Random();
+    int result = rnd.Next(0, 2);
+    if (result == 1)
+    {
+        Console.WriteLine("Tails!");
+        tails = tails + 1;
+        count = count + 1;
+
+    }
+    else if (result == 0)
+    {
+        Console.WriteLine("Heads!");
+        heads = heads + 1;
+        count = count + 1;
+    }
+}
+Console.WriteLine("Heads Percentage = " + heads + " ,Tails Percentage = " + tails + " ,Counts = " + count);
+Console.ReadLine();
